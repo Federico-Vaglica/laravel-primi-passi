9632d31d44300f7214f6ddfeaecb855ec4c18d36
federicov <vaglicafederico95@gmail.com>
2020-10-08 15:53:29 +0200

    Logic parte done

--- laravel-project/resources/views/partials/header.blade.php
+++ laravel-project/resources/views/partials/header.blade.php
@@ -4,14 +4,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <title>La Molisana</title>
 </head>
 <body>
     <header>
-        <img src='https://www.lamolisana.it/wp-content/uploads/2020/05/marchio-sito-test.png' alt="la molisana">
-        <ul>
-            <li><a href="{{route('home')}}">Home</a></li>
-            <li><a href="{{route('prodotti')}}">prodotti</a></li>
-            <li><a href="{{route('contatti')}}">contatti</a></li>
-        </ul>
+        <div class='logo'>
+            <img src='https://www.lamolisana.it/wp-content/uploads/2020/05/marchio-sito-test.png' alt="la molisana">
+        </div>
+        <nav>
+            <div class='container'>
+                <ul>
+                    <li><a href="{{route('home')}}">Home</a></li>
+                    <li><a href="{{route('prodotti')}}">Prodotti</a></li>
+                    <li><a href="{{route('contatti')}}">Contatti</a></li>
+                </ul>
+            </div>
+
+        </nav>
+
     </header>
--- laravel-project/resources/views/partials/main_prodotti.blade.php
+++ laravel-project/resources/views/partials/main_prodotti.blade.php
@@ -1,9 +1,10 @@
 <main>
-    <div class="cards">
-        <div>
-            <h2>le lunghe</h2>
+    <div class="container">
+        <h2>le lunghe</h2>
+        <div class="cards">
+            
             @foreach ($lunghe as $card)
-                <div class="card">
+            <div class="card">
                 <h3>{{$card['titolo']}}</h3>
                 <img src="{{$card['src']}}" alt="{{$card['titolo']}}">
                 </div>
@@ -12,7 +13,7 @@
         <div>
             <h2>le corte</h2>
             @foreach ($corte as $card)
-                <div class="card">
+            <div class="card">
                 <h3>{{$card['titolo']}}</h3>
                 <img src="{{$card['src']}}" alt="{{$card['titolo']}}">
                 </div>
@@ -21,7 +22,7 @@
         <div>
             <h2>le cortissime</h2>
             @foreach ($cortissime as $card)
-                <div class="card">
+            <div class="card">
                 <h3>{{$card['titolo']}}</h3>
                 <img src="{{$card['src']}}" alt="{{$card['titolo']}}">
                 </div>
